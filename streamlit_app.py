78456b38caff85af13fef4af15179ada2f320955
NagurvaliShaik <146914597+NagurvaliShaik@users.noreply.github.com>
2023-10-18 16:59:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('🥣 Breakfast favorites')
@@ -8,7 +12,7 @@ streamlit.text('🥑🍞 Hard- Boiled Free - Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -20,7 +24,7 @@ streamlit.dataframe(my_fruit_list)
 #streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index),['Avocado','strawberries'])
 #streamlit.dataframe(my_fruit_list)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
@@ -28,7 +32,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
 # write your own comment -what does the next line do? 
@@ -39,7 +43,7 @@ streamlit.dataframe(fruityvice_normalized)
 #don't run anything past here while troubleshoot 
 streamlit.stop()
 
-import snowflake.connector
+#import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
